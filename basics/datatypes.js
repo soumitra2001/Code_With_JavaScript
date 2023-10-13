a68e5acb16c522b90f56b8ef9b75c03d5637304c
Supriya Ghorai <78735395+soumitra2001@users.noreply.github.com>
2023-10-13 16:58:13 +0000

    new file added

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// alert("Hello") // We are using nodejs, not browser
+
+console.log(3+4)
+
+console.log("Hello Supriya")
+
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// undefined 
+// symbol => unique
+
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
