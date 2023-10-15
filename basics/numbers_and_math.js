9fd3fc40483eaf795e28ca6fe6be0564d45bdd66
Supriya Ghorai <78735395+soumitra2001@users.noreply.github.com>
2023-10-15 17:55:15 +0000

    math props

---
+++ basics/numbers_and_math.js
@@ -0,0 +1,19 @@
+const amount = 200
+
+// console.log(amount);
+
+const balance = new Number(320)
+
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+console.log(typeof Math);
+
+// Random number in 1-10
+console.log(Math.floor(Math.random()*10)+1);
+
+// Random Number from (Min to Max) =>
+let min = 10, max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
