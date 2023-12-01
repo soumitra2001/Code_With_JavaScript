4493ac1d5ababa96b89a8dcae73851b0d796303b
Supriya Ghorai <78735395+soumitra2001@users.noreply.github.com>
2023-12-01 08:59:59 +0000

    project_01

---
+++ projects/color_changer/changeColor.js
@@ -0,0 +1,27 @@
+const buttons=document.querySelectorAll(".button");
+const body=document.querySelector('body');
+// console.log(body);
+
+buttons.forEach(function(button){
+    // console.log(button);
+
+    button.addEventListener('click',function(e){
+        console.log(e);
+        const color = e.target.id;
+        switch(color){
+            case 'gray':
+                body.style.backgroundColor=color;
+                // break;
+            case 'black':
+                body.style.backgroundColor=color;
+                body.style.textDecorationColor='white';
+                // break;
+            case 'blue':
+                body.style.backgroundColor=color;
+                // break;
+            case 'yellow':
+                body.style.backgroundColor=color;
+                // break;
+        }
+    });
+});
